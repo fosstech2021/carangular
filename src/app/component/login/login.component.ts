36fec50b424be2a338ed328916c1d33e97d64d9e
lawson borges <lawsonborges@gmail.com>
2021-03-06 01:49:32 +0530

    Googlelogin

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -33,22 +33,28 @@ export class LoginComponent implements OnInit {
       email: ["", Validators.required],
       pwd: ["", Validators.required],
     });
-    this.googleSDK();
+
+    this.googleInitialize();
+    // this.googleSDK();
+    // window["onSignIn"] = user => (()=> {
+    //   this.afterSignup(user)
+    // })
   }
 
-  googleSDK() {
+  googleInitialize() {
     window["googleSDKLoaded"] = () => {
       window["gapi"].load("auth2", () => {
-        this.auth2 = window["gapi"].auth2.getAuthInstance({
-          client_id: "AIzaSyCAU8eYPMz-D91D-IQNwq0cF9UsjvkTL9E",
-          cookiepolicy: "single_host_origin",
+        this.auth2 = window["gapi"].auth2.init({
+          client_id:
+            "Key",
+          cookie_policy: "single_host_origin",
           scope: "profile email",
         });
-        this.prepareLoginButton(document.getElementById("googleBtn"));
+        this.prepareLogin();
       });
     };
-
     (function (d, s, id) {
+      debugger;
       var js,
         fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {
@@ -60,40 +66,50 @@ export class LoginComponent implements OnInit {
       fjs.parentNode.insertBefore(js, fjs);
     })(document, "script", "google-jssdk");
   }
-  prepareLoginButton(element) {
-    var profile;
+
+  prepareLogin() {
     this.auth2.attachClickHandler(
-      element,
+      this.loginElement.nativeElement,
       {},
       (googleUser) => {
-        profile = googleUser.getBasicProfile();
-        var IsGoogleLogin = "1";
-        var obj = new FormData();
+        debugger;
+        let profile = googleUser.getBasicProfile();
+        let profile1 = googleUser.uc;
+        // console.log("Token || " + googleUser.getAuthResponse().id_token);
 
-        obj.append("Email", profile.getEmail());
-        obj.append("ImageURL", profile.getImageUrl());
-        obj.append("Name", profile.getName());
-        obj.append("FirstName", profile.getGivenName());
-        obj.append("LastName", profile.getFamilyName());
-        obj.append("SocialMediaID", profile.getId());
-        obj.append("SocialMediaToken", googleUser.getAuthResponse().id_token);
-        obj.append("IsGoogleLogin", IsGoogleLogin);
+        // console.log("Image URL: " + profile.getImageUrl());
+        // console.log("Email: " + profile.getEmail());
 
-        this.ngZone.run(() => {
-          // this.register.loginVerification(obj).subscribe((res: any) => {
-          // sessionStorage.setItem("ConsumerId", res.ConsumerId);
-          // this.LoaderService.login(this.ConsumerID);
-          // if (res.Err == true) {
-          //   this.ErrMsg = res.ErrMsg;
-          // } else {
-          //   $("#signIn").toast("show");
-          //   this.UrlNav = localStorage.getItem("url");
-          //   this.router.navigateByUrl(this.UrlNav);
-          // }
-          // });
-        });
+      
+       
+          var obj = {
+            email: profile.nt,
+            password: "",
+            first_name: profile.QS ,
+            last_name: profile.SQ ,
+            username: profile.sd,
+            channel: "2",
+            social_auth_token: profile1.access_token,
+          };
+      
+          this.register.RegisterUser(obj).subscribe((res: any) => {
+            if (res.success) {
+              this.register.Setlogindetail(res.data);
+              window.location.href = "/home";
+
+            } else {
+              window.location.href = "/home";
+            }
+        
+          });
+        
+
+
+
       },
-      (error) => {}
+      (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      }
     );
   }
 
@@ -107,11 +123,81 @@ export class LoginComponent implements OnInit {
     };
 
     this.register.postLogin(obj).subscribe((res: any) => {
-      this.register.Setlogindetail(res.data)
-      
+      this.register.Setlogindetail(res.data);
+
       //this.router.navigate(['/home']);
-      window.location.href="/home";
+      window.location.href = "/home";
     });
   }
-  registernew() {}
+
+  //   afterSignup(user)
+  //   {
+  //     debugger;
+  // console.log(user)
+  //   }
+  //   googleSDK() {
+  //     window["googleSDKLoaded"] = () => {
+  //       window["gapi"].load("auth2", () => {
+  //         this.auth2 = window["gapi"].auth2.getAuthInstance({
+  //           client_id: "AIzaSyCAU8eYPMz-D91D-IQNwq0cF9UsjvkTL9E",
+  //           cookiepolicy: "single_host_origin",
+  //           scope: "profile email",
+  //         });
+  //         this.prepareLoginButton(document.getElementById("googleBtn"));
+  //       });
+  //     };
+
+  //     (function (d, s, id) {
+  //       var js,
+  //         fjs = d.getElementsByTagName(s)[0];
+  //       if (d.getElementById(id)) {
+  //         return;
+  //       }
+  //       js = d.createElement(s);
+  //       js.id = id;
+  //       js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+  //       fjs.parentNode.insertBefore(js, fjs);
+  //     })(document, "script", "google-jssdk");
+  //   }
+
+  // prepareLoginButton(element) {
+  //   debugger;
+  //   var profile;
+  //   debugger;
+  //   this.auth2.attachClickHandler(
+
+  //     element,
+  //     {},
+  //     (googleUser) => {
+  //       debugger;
+  //       profile = googleUser.getBasicProfile();
+  //       var IsGoogleLogin = "1";
+  //       var obj = new FormData();
+
+  //       obj.append("Email", profile.getEmail());
+  //       obj.append("ImageURL", profile.getImageUrl());
+  //       obj.append("Name", profile.getName());
+  //       obj.append("FirstName", profile.getGivenName());
+  //       obj.append("LastName", profile.getFamilyName());
+  //       obj.append("SocialMediaID", profile.getId());
+  //       obj.append("SocialMediaToken", googleUser.getAuthResponse().id_token);
+  //       obj.append("IsGoogleLogin", IsGoogleLogin);
+
+  //       this.ngZone.run(() => {
+  //         // this.register.loginVerification(obj).subscribe((res: any) => {
+  //         // sessionStorage.setItem("ConsumerId", res.ConsumerId);
+  //         // this.LoaderService.login(this.ConsumerID);
+  //         // if (res.Err == true) {
+  //         //   this.ErrMsg = res.ErrMsg;
+  //         // } else {
+  //         //   $("#signIn").toast("show");
+  //         //   this.UrlNav = localStorage.getItem("url");
+  //         //   this.router.navigateByUrl(this.UrlNav);
+  //         // }
+  //         // });
+  //       });
+  //     },
+  //     (error) => {}
+  //   );
+  // }
 }
